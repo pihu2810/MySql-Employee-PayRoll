2b56147aeb15741708e25ab6b0942e0cc06321c5
pihu2810 <vishaka.kadam01@gmail.com>
2022-05-04 21:51:57 +0530

    Refacted to create Emp salary id name startdate

--- EmpPayRoll.sql
+++ EmpPayRoll.sql
@@ -1,1 +1,4 @@
 create database payroll_service;
+show databases;
+use payroll_service;
+create Table Employee_payroll(id int PRIMARY KEY,EmpName VARCHAR(100),EmpSalary INT,EmpStartDate VARCHAR(255));
