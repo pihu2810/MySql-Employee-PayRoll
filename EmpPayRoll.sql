14f98ee4d263db76fd2bff9244c7a3c059501104
pihu2810 <vishaka.kadam01@gmail.com>
2022-05-04 22:38:55 +0530

    Refacted to select cast now employee payroll

--- EmpPayRoll.sql
+++ EmpPayRoll.sql
@@ -8,3 +8,6 @@ insert into Employee_payroll value(3,"kadam",25000,28-06-2019);
 insert into Employee_payroll value(4,"shanvika",10000,4-01-2016);
 insert into Employee_payroll value(5,"pihu",15000,13-07-2018);
 SELECT * FROM payroll_service.employee_payroll;
+select * from Employee_payroll where EmpName = 'vishaka';
+select * from Employee_payroll
+where EmpStartDate between '24-05-2020' and date(now());
