9f508d933573740aa26904d0d921d02b1258e1ec
pihu2810 <vishaka.kadam01@gmail.com>
2022-05-04 23:11:37 +0530

    Refacted to alter phonenumber address employee payroll

--- EmpPayRoll.sql
+++ EmpPayRoll.sql
@@ -27,4 +27,9 @@ select gender,max(EmpSalary) from Employee_payroll group by gender;
 select gender,min(EmpSalary) from Employee_payroll group by gender;
 
 select gender,count(*) from Employee_payroll group by gender;
-
+alter table Employee_payroll add phone_number varchar(13) after EmpName;
+alter table Employee_payroll add address varchar(200) after phone_number;
+alter table Employee_payroll add department varchar(100) not null after address;
+alter table Employee_payroll alter address set default 'TBD';
+insert into Employee_payroll (id,EmpName,department,EmpSalary,EmpStartDate,gender) 
+values(7,'Kunal','CSE', 49000, '13-08-20014','M');
