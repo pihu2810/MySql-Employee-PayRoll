8db4e73e10427901e90a415e0e4f4a115f12aab4
pihu2810 <vishaka.kadam01@gmail.com>
2022-05-04 22:23:17 +0530

    Refacted to insert id salary name startdate

--- EmpPayRoll.sql
+++ EmpPayRoll.sql
@@ -2,3 +2,8 @@ create database payroll_service;
 show databases;
 use payroll_service;
 create Table Employee_payroll(id int PRIMARY KEY,EmpName VARCHAR(100),EmpSalary INT,EmpStartDate VARCHAR(255));
+insert into Employee_payroll value(1,"vishaka",30000,24-05-2020);
+insert into Employee_payroll value(2,"vishu",20000,14-05-2021);
+insert into Employee_payroll value(3,"kadam",25000,28-06-2019);
+insert into Employee_payroll value(4,"shanvika",10000,4-01-2016);
+insert into Employee_payroll value(5,"pihu",15000,13-07-2018);
