625e69137d45f000bd17229d7232e838ca2acd6f
pihu2810 <vishaka.kadam01@gmail.com>
2022-05-05 07:49:11 +0530

    Refacted to create ER diagram

--- EmpPayRoll.sql
+++ EmpPayRoll.sql
@@ -44,3 +44,40 @@ values(8,'Terissa','9888888888','hyd','Sales','F',4800000,100000,40000,5400,4000
 insert into Employee_payroll 
        (id,EmpName,phone_number,address,department,gender,basic_pay,deductions,taxable_pay,tax,net_pay,EmpstartDate)
 values(9,'Terissa','9888888888','pune','Marketing','F',4800000,100000,40000,5400,4000000,'23-08-2021');
+create table employee(
+id int not null auto_increment primary key,
+name varchar(50) not null,
+gender varchar(1),
+phone_number varchar(13),
+address varchar(200),
+startDate Date not null
+);
+desc employee;
+
+create table payroll(
+id int not null auto_increment primary key,
+basic_pay double,
+deductions double,
+income_tax double,
+taxable_pay double,
+tax double,
+net_pay double,
+emp_ID int,
+foreign key(emp_ID) references employee(id)
+);
+desc payroll;
+
+create table department(
+  id int not null auto_increment primary key,
+  dept_name varchar(20) not null
+  );
+  desc department;
+
+create table employee_department(
+  employee_id int not null,
+  department_id int not null,
+  foreign key(employee_id) references employee(id),
+  foreign key(department_id) references department(id),
+  primary key(employee_id,department_id)
+);
+desc employee_department;
